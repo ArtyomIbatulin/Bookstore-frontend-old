9060a342dd1d8c92586528a52805f58bc99620b7
Artyom Ibatulin <artem.alekseevich@bk.ru>
2021-05-11 17:53:21 +0300

    deleted material ui, tuning styled component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledTitle = styled.h1`
+  text-align: center;
+`;
+
 function App() {
-  return (
-    <div className="App">
-      <h1>Bookstore</h1>
-    </div>
-  );
+  return <StyledTitle>Book Store</StyledTitle>;
 }
 
 export default App;

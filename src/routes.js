9c5e5e4af9234534190f982ed11f897cbde52423
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2021-11-24 20:38:12 +0300

    added routes

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,13 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import Wishlist from './pages/Wishlist';
-import MainPage from './pages/MainPage';
-import BookPage from './pages/BookPage';
-import Profile from './pages/Profile';
-import Orders from './pages/Orders';
-import AdminPage from './pages/AdminPage';
-import SignIn from './pages/signIn/SignIn';
-import SignUp from './pages/signUp/SignUp';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import Wishlist from "./pages/Wishlist";
+import MainPage from "./pages/MainPage";
+import BookPage from "./pages/BookPage";
+import Profile from "./pages/Profile";
+import Orders from "./pages/Orders";
+import AdminPage from "./pages/AdminPage";
+import SignIn from "./pages/signIn/SignIn";
+import SignUp from "./pages/signUp/SignUp";
 
 const useRoutes = (isAuthenticated) => {
   if (isAuthenticated) {
@@ -27,6 +27,8 @@ const useRoutes = (isAuthenticated) => {
   return (
     <Switch>
       <Route exact path="/" component={MainPage} />
+      <Route path="/wishlist" component={Wishlist} />
+      <Route path="/book/:id" component={BookPage} />
       <Route path="/sign-in" component={SignIn} />
       <Route path="/sign-up" component={SignUp} />
       <Redirect to="/" />

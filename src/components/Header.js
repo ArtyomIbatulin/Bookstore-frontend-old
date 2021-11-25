d68c07fa3d436cf9a6e68269a24046e9c35694c6
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2021-11-25 19:33:39 +0300

    styled header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+// import { Link } from "react-router-dom";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import FavoriteIcon from "@mui/icons-material/Favorite";
 
 const Header = () => {
   // const history = useHistory();
@@ -12,7 +21,7 @@ const Header = () => {
 
   return (
     <header>
-      <nav>
+      {/* <nav>
         <div>
           <Link to={'/'}>
             <img alt="Logo" />
@@ -24,13 +33,45 @@ const Header = () => {
 
           <Link to="/sign-up">
             <span className="nav-link">sign-up</span>
-          </Link>
+          </Link> */}
 
-          {/* <Link to='/' onClick={logoutHandler}>
+      {/* <Link to='/' onClick={logoutHandler}>
                   <span className='nav-link'>Выйти</span>
                 </Link> */}
-        </div>
-      </nav>
+      {/* </div>
+      </nav> */}
+
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              My Bookstore
+            </Typography>
+
+            <IconButton color="inherit">
+              <FavoriteIcon />
+            </IconButton>
+
+            <IconButton color="inherit">
+              <ShoppingCartIcon />
+            </IconButton>
+
+            <Button color="inherit">Sign in</Button>
+
+            <Button color="inherit">Sign up</Button>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </header>
   );
 };

9c33046f2d5470957c347ab1fe294b7c729a7a4d
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2021-11-26 16:59:48 +0300

    added db file

---
+++ src/db.js
@@ -0,0 +1,44 @@
+const books = [
+  {
+    id: 1,
+    name: "С жизнью наедине",
+    price: 3200,
+    description: "Про жизнь",
+    img: "1aa362d6-4c45-4cb5-8f83-ac9d40b8aefa.jpg",
+    Authors: "Толкиен",
+    Categories: "Фантастика",
+    Ratings: "",
+  },
+  {
+    id: 2,
+    name: "Властелин колец 1",
+    price: 112,
+    description: "Про хоббитов",
+    img: "9d4716a6-03b9-4999-b069-5461ffce71ea.jpg",
+    Authors: "Толкиен",
+    Categories: "Фантастика",
+    Ratings: "",
+  },
+  {
+    id: 3,
+    name: "Властелин колец 2",
+    price: 1121,
+    description: "Про хоббитов 2",
+    img: "038b2f64-e325-403f-8ce0-caa5a953b45f.jpg",
+    Authors: "Кристин Ханна",
+    Categories: "Детектив",
+    Ratings: "",
+  },
+  {
+    id: 4,
+    name: "Tesla",
+    price: 503,
+    description: "Книга Илона Маска",
+    img: "2043c564-ae02-4408-a7bb-e8aab6fa8ca9.jpg",
+    Authors: "Илон Маск",
+    Categories: "Фантастика",
+    Ratings: "",
+  },
+];
+
+export { books };
